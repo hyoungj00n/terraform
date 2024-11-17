a5b8e06976ed77386e2dbe12e8a98281821f0d09
joon <khjoon372@naver.com>
2024-11-18 00:38:44 +0900

    add security group

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-0f3a440bbcff3d043" 
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+  user_data_replace_on_change = true
+  
+
+  tags  =  { 
+    Name  =  "terraform"  
+  } 
+}
+
+resource  "aws_security_group" "instance"  {
+  name  =  "terraform-instance" 
+  
+  ingress  { 
+    from_port    =  8080 
+    to_port      =  8080
+    protocol     =  "tcp"
+    cidr_blocks  =  [ "0.0.0.0/0" ] 
+  }
+}
+
+output "public_ip" {
+ value = aws_instance.terraform.public_ip
+ description = "The public IP address of the web server"
+}
--- main.tf
+++ main.tf
@@ -2,7 +2,3 @@ provider  "aws" {
   region = "ap-northeast-2" 
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f3a440bbcff3d043" 
-  instance_type = "t2.micro"
-}
